ba81f620f3e8618b852954dd040b08d1dad3150e
user <1@test.test>
2018-12-07 01:46:01 +0300

    fix: test settings

--- base/UrlBaseObject.php
+++ base/UrlBaseObject.php
@@ -17,27 +17,27 @@ class UrlBaseObject
      * Number of all sql queries for 1 api request
      * Good value: 30
      */
-    const MAX_TOTAL_QUERIES_COUNT = 60;
+    const MAX_TOTAL_QUERIES_COUNT = 30;
     /**
      * Duration of all sql queries in milliseconds
-     * Good value: 100
+     * Good value: 150
      */
-    const MAX_TOTAL_QUERIES_TIME = 300;
+    const MAX_TOTAL_QUERIES_TIME = 150;
     /**
      * Total number of duplicates
-     * Good value: 1 (max 2)
+     * Good value: 1 (max 6)
      */
-    const MAX_TOTAL_DUPLICATES = 20;
+    const MAX_TOTAL_DUPLICATES = 6;
     /**
      * Max duration for 1 sql query
      * Good value: 30
      */
-    const MAX_QUERY_TIME = 130;
+    const MAX_QUERY_TIME = 30;
     /**
      * Number of duplicates for one sql query
-     * Good value: 1 (max 2)
+     * Good value: 1 (max 6)
      */
-    const MAX_QUERY_DUPLICATE = 20;
+    const MAX_QUERY_DUPLICATE = 6;
 
     /**
      * @return array
